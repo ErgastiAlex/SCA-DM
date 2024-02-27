b7b945d8cf98f032a893a4d935ff3f31ed5809b8
Alex Ergasti <ergasti.alex@gmail.com>
2024-02-27 13:36:08 +0000

    Add support for new datasets

--- dataset.py
+++ dataset.py
@@ -65,6 +65,89 @@ class CelebAHQ(FacesBase):
 
         return {"image":im, "label":label}
 
+class Cityscape(FacesBase):
+    def __init__(self, size, path, prefix="train"):
+        super().__init__()
+        self.size=size
+        images_path=os.path.join(path,"leftImg8bit", prefix)
+        labels_path=os.path.join(path,"gtFine", prefix)
+
+        #images path contains subfolders
+        images=[]
+        labels=[]
+        for root, dirs, files in os.walk(images_path):
+            for file in files:
+                images.append(os.path.join(root, file)) 
+                file_prefix = file.split("_leftImg8bit.png")[0]
+                labels.append(os.path.join(labels_path, root.split("/")[-1], file_prefix+"_gtFine_labelIds.png"))
+
+        self.images = images
+        self.labels = labels
+        
+        assert len(self.images) == len(self.labels)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, i):
+        img_name=self.images[i]
+        label_name=self.labels[i]
+
+        im = Image.open(img_name)
+        im = im.resize((self.size*2,self.size))
+        im = np.array(im).astype(np.uint8)
+        # im = np.transpose(im, (2, 0, 1))
+        im = (im/127.5 - 1.0).astype(np.float32)
+
+        label = Image.open(label_name)
+        label = label.resize((self.size*2,self.size), Image.NEAREST) #width, height
+        label = np.array(label).astype(np.uint8)
+        label = label[...]
+        label = torch.from_numpy(label).to(torch.int64)
+
+        return {"image":im, "label":label}
+
+class Ade20K(FacesBase):
+    def __init__(self, size, path, prefix="training"):
+        super().__init__()
+        self.size=size
+        images_path=os.path.join(path,"images", prefix)
+        labels_path=os.path.join(path,"annotations", prefix)
+
+        #images path contains subfolders
+        images=os.listdir(images_path)
+        labels=os.listdir(labels_path)
+    
+        images.sort(key = lambda x: int(x.split("_")[-1].split(".")[0]))
+        labels.sort(key = lambda x: int(x.split("_")[-1].split(".")[0]))
+
+        self.images = [os.path.join(images_path, img) for img in images]
+        self.labels = [os.path.join(labels_path, lbl) for lbl in labels]
+        
+        assert len(self.images) == len(self.labels)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, i):
+        img_name=self.images[i]
+        label_name=self.labels[i]
+
+        im = Image.open(img_name)
+        im = im.resize((self.size,self.size))
+        im = np.array(im).astype(np.uint8)
+        # im = np.transpose(im, (2, 0, 1))
+        im = (im/127.5 - 1.0).astype(np.float32)
+
+        label = Image.open(label_name)
+        label = label.resize((self.size,self.size), Image.NEAREST) #width, height
+        label = np.array(label).astype(np.uint8)
+
+        label = label[...]
+        label = torch.from_numpy(label).to(torch.int64)
+
+        return {"image":im, "label":label}
+
 
 class FFHQTrain(FacesBase):
     def __init__(self, size, keys=None):
